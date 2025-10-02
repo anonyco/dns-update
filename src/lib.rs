4f7e0faa153eda8a36f8e6c6070151dfacb81dca
jack <25795277+anonyco@users.noreply.github.com>
2025-10-02 11:17:25 -0400

    Add tests for the dns record trait and API cache manager
    Signed-off-by: Jack Giffin <jack.giffin.dev@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub mod http;
 pub mod providers;
 pub mod tests;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, thiserror::Error)]
 pub enum Error {
     Protocol(String),
     Parse(String),
